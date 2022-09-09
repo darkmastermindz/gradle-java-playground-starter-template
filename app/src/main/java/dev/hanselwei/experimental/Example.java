3cd6faf2829f3a458b3d2754cb3615dba904d426
Hansel Wei <hello@hanselwei.dev>
2022-09-09 04:25:32 +0000

    init template

---
+++ app/src/main/java/dev/hanselwei/experimental/Example.java
@@ -0,0 +1,5 @@
+package dev.hanselwei.experimental;
+
+public static void main(String[] args) {
+    System.out.println("Hello World! I'm a experiment. Made with <3 by Hansel Wei @darkmastermindz");
+}
---
+++ app/src/main/java/dev/hanselwei/experimental/data/Playground.java
@@ -0,0 +1,12 @@
+package dev.hanselwei.experimental.data;
+
+import lombok.data;
+
+/**
+ * Playground
+ */
+@Data
+public class Playground {
+    String name;
+    int capacity;
+}
