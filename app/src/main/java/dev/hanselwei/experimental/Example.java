154b26f0320c582c806e0e01dd45705eb6082328
Hansel Wei <hello@hanselwei.dev>
2023-02-07 08:48:13 -0500

    Update Example.java

--- app/src/main/java/dev/hanselwei/experimental/Example.java
+++ app/src/main/java/dev/hanselwei/experimental/Example.java
@@ -1,5 +1,7 @@
 package dev.hanselwei.experimental;
 
-public static void main(String[] args) {
-    System.out.println("Hello World! I'm a experiment. Made with <3 by Hansel Wei @darkmastermindz");
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("Hello World! I'm a experiment. Made with <3 by Hansel Wei @darkmastermindz");
+    }
 }
